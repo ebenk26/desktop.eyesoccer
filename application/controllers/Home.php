6f5ef310d5d85bedebf421441d1debe1038d49f4
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-05-17 12:39:05 +0700

    penambahan helper meta_tag by waldy

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,10 +34,7 @@ class Home extends CI_Controller
 
         }
         else{
-        $data["meta"]["title"] = "";
-        $data["meta"]["image"] = base_url() . "/assets/img/tab_icon.png";
-        $data["meta"]["description"] = set_meta('',['url'=> base_url(),'result'=> 'desc']);
-
+        $data["meta"]['share'] = set_meta(['uri'=> 'eyenews']);
         $data['jadwal'] = $this->Home_model->get_all_jadwal();
         $data['jadwal_2'] = $this->Home_model->get_all_jadwal_2();
         $data['trend_eyetube'] = $this->Home_model->get_trending_eyetube();
@@ -658,16 +655,6 @@ class Home extends CI_Controller
     public function search()
     {
         $data['kanal'] = "home";
-        $search = $this->input->get('q');
-
-        $data['eyenews'] = $this->mod->getAll('tbl_eyenews', $where = array(), $select = array('title', 'description', 'url', 'thumb1'), $order = array('eyenews_id' => 'desc'), $limit = '', $offset = '', $whereNotin = array(), $like = array('title' => $search));
-
-        $data['eyetube'] = $this->mod->getAll('tbl_eyetube', $where = array(), $select = array('title', 'description', 'url', 'thumb1'), $order = array('eyetube_id' => 'desc'), $limit = '', $offset = '', $whereNotin = array(), $like = array('title' => $search));
-
-        $data['player'] = $this->Home_model->getAllLeftJoin('tbl_player', $where = array(), $select = array('tbl_player.name as name', 'tbl_player.position as position', 'tbl_player.number as number', 'tbl_player.url as url', 'tbl_player.pic as pic', 'tbl_club.name as club'), $order = array('tbl_player.player_id' => 'desc'), $limit = '', $offset = '', $whereNotin = array(), $like = array('tbl_player.name' => $search), $leftjoin = array('tbl_club' => 'tbl_club.club_id = tbl_player.club_id'));
-
-        $data['club'] = $this->Home_model->getAllLeftJoin('tbl_club', $where = array(), $select = array('name', 'address', 'stadium', 'url', 'logo'), $order = array('club_id' => 'desc'), $limit = '', $offset = '', $whereNotin = array(), $like = array('name' => $search), $leftjoin = array());
-
         $data["body"] = $this->load->view('home/search', $data, TRUE);
         $this->load->view('template/static', $data);
     }
