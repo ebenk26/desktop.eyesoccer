d9d1c529a141aa762fed7a68e6a9342e097d002b
Kharizuno <kharizuno@yahoo.com>
2018-06-07 11:23:09 +0700

    'p'

--- application/views/eyeprofile/ajax/clublist.php
+++ application/views/eyeprofile/ajax/clublist.php
@@ -1,76 +1,82 @@
-<?php 
+<?php
 $res = json_decode($res);
 // p($res);
 // exit;
 
 
-echo "<div class='ep2box fl-l pd-t-20'>";		
+echo "<div class='ep2box fl-l pd-t-20'>";
 
-	foreach($res->data as $r):
-	$url = preg_match("/[^\/]+$/", $r->url_logo, $matches);
-	echo '<a href="'.CLUBDETAIL.$r->slug.'" style="text-decoration:unset;color:#424242;">'.'<div class="box-content ep2 fl-l">';
-			echo '<img src="'.(count($matches) == 0 ? imgCache('LOGO UNTUK APLIKASI.jpg') : $r->url_logo).'" alt="" >';
-			echo '<div class="detail">';
-				echo '<h2>'.$r->name.'</h2>';
-				echo '<h3>'.$r->competition.'</h3>';
-				echo "<table>
+foreach ($res->data as $r):
+    $url = preg_match("/[^\/]+$/", $r->url_logo, $matches);
+    echo '<a href="' . CLUBDETAIL . $r->slug . '" style="text-decoration:unset;color:#424242;">' . '<div class="box-content ep2 fl-l">';
+    echo '<img src="' . (count($matches) == 0 ? imgCache('LOGO UNTUK APLIKASI.jpg') : $r->url_logo) . '" alt="" >';
+    echo '<div class="detail">';
+    echo '<h2>' . $r->name . '</h2>';
+    echo '<h3>' . $r->competition . '</h3>';
+    echo "<table>
 					<tr>
 						<td>Squad</td>";
-						echo "<td>: ".$r->number_of_player."</td>";
-					echo "</tr>
+    echo "<td>: " . $r->number_of_player . "</td>";
+    echo "</tr>
 					<tr>";
-						// echo "<td>Manager</td>";
-						// echo "<td>: ".$r->manager."</td>";
-						//getManager($r->id_club)
-					echo "</tr>
+    // echo "<td>Manager</td>";
+    // echo "<td>: ".$r->manager."</td>";
+    //getManager($r->id_club)
+    echo "</tr>
 				</table>
 			</div>
 		</div>
 	</a>";
-	endforeach;
-	
-echo "</div>";?>
+endforeach;
+
+echo "</div>"; ?>
 
 
 <?php
 //pagination 
 
-$cc  = json_decode($count)->data;
+$cc = json_decode($count)->data;
 $totalRows = $cc[0]->cc;
 $limit = $res->query->limit;
-(int) $page  = $res->query->page;
-$totalPage = ceil($totalRows/$limit); //total page
+(int)$page = $res->query->page;
+$totalPage = ceil($totalRows / $limit); //total page
 $competition = $res->query->competition;
-$league = ($res->query->league == 'SSB / Akademi Sepakbola' ? 'SSB' :  $res->query->league);
-$uri = ($competition == null ? 'Liga Indonesia 1' : ($league != null || $league != '' ? $competition.'/'.$league : $competition));
-;?>
+$league = ($res->query->league == 'SSB / Akademi Sepakbola' ? 'SSB' : $res->query->league);
+$uri = ($competition == null ? 'Liga Indonesia 1' : ($league != null || $league != '' ? $competition . '/' . $league : $competition));; ?>
 
 
 <div class="pull-right">
-	
-  <ul class="orange-default">
-  	<?php echo ($page > 1 ? '<li>'.anchor(pCLUB.$uri.'/page/'.($page-1) ,'Sebelumnya','display="block"').'</li>' : '')?>
-    <?php  
-	    if($page > 4 ){?>
 
-	    <li><?php echo anchor(pCLUB.$uri.'/page/1','1')?></li>
-	    <li>...</li>
-    <?php }
-   
-    for($i=($page-2); $i < ($page+2);$i++):
+    <ul class="orange-default">
+        <?php echo($page > 1 ? '<li>' . anchor(pCLUB . $uri . '/page/' . ($page - 1), 'Sebelumnya', 'display="block"') . '</li>' : '') ?>
 
-    if($i < 1 ){ continue; }
-    if($i > $totalPage){ break;}?>
+        <?php
+        if ($page > 4) {
+            ?>
+            <li><?php echo anchor(pCLUB . $uri . '/page/1', '1') ?></li>
+            <li>...</li>
+            <?php
+        }
 
-    	<li <?php echo ($page == $i ? 'class="active"' : '')?>><?php echo  anchor(pCLUB.$uri.'/page/'.$i,$i)?></li>
+        for ($i = ($page - 2); $i < ($page + 2); $i++):
 
-    <?php endfor;?>
-    <?php if($totalPage > $page AND $page > 3){?>
-		<li>...</li>
-		<li><?php echo anchor(pCLUB.$uri.'/page/'.$totalPage,$totalPage) ?></li>
-    <?php }?>
+            if ($i < 1) {
+                continue;
+            }
+            if ($i > $totalPage) {
+                break;
+            }
 
-   <?php echo ($page < $totalPage ?'<li>'.anchor(pCLUB.$uri.'/page/'.($page > 0 ? ($page+1) : $page),'Selanjutnya').'</li>' : '')?>
-    
-  </ul>
+            ?>
+            <li <?php echo($page == $i ? 'class="active"' : '') ?>><?php echo anchor(pCLUB . $uri . '/page/' . $i, $i) ?></li> <?php
+        endfor;
+        ?>
+
+        <?php if ($totalPage > $page AND $page > 3) { ?>
+            <li>...</li>
+            <li><?php echo anchor(pCLUB . $uri . '/page/' . $totalPage, $totalPage) ?></li>
+        <?php } ?>
+
+        <?php echo($page < $totalPage ? '<li>' . anchor(pCLUB . $uri . '/page/' . ($page > 0 ? ($page + 1) : $page), 'Selanjutnya') . '</li>' : '') ?>
+    </ul>
 </div>
